69ad666e84c1fea72d13cca02c9bab54c9c4319d
nhatminh0811 <gwimloveu@gmail.com>
2026-05-17 09:50:14 +0700

    Serve media files in production via explicit route

--- brfn/urls.py
+++ brfn/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve as static_serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -20,5 +21,10 @@ urlpatterns = [
     path("api/", include("products.api_urls")),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        path(f"{settings.MEDIA_URL.lstrip('/')}<path:path>", static_serve, {"document_root": settings.MEDIA_ROOT}),
+    ]
 
